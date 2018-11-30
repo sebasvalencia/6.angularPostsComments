8eb28c59846764a1bfec6dbf3a3938a5ac0812f9
Sebastian Valencia <sebastian.valencia@yuxiglobal.com>
2018-11-30 13:27:42 -0500

    Create project and user the two services posts and comments

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,40 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angularPostsComents';
+
+  postUrl = 'https://jsonplaceholder.typicode.com/posts';
+  commentsUrl = 'https://jsonplaceholder.typicode.com/comments/';
+
+  posts$: Observable<Post>;
+  comments$: Observable<Comment>;
+  isComment = false;
+
+  constructor(private http: HttpClient) {
+    this.posts$ = this.http.get<Post>(this.postUrl);
+    this.comments$ = this.http.get<Comment>(this.commentsUrl);
+    // this.http.get(this.commentsUrl).subscribe(console.log);
+  }
+
+  onShowComments(id) {
+    // console.log('id: ', id);
+    this.isComment = true;
+    this.comments$ = this.http.get<Comment>(this.commentsUrl + '?postId=' + id);
+  }
+
 }
+
+interface Post {
+  body: string;
+  id: number;
+  title: string;
+  userId: number;
+}
+
+interface Comment {
+  body: string;
+  email: string;
+  id: number;
+  name: string;
+  postId: number;
+}
+
